b40e318ae787e19be5ed6e53ad227db9a1a8c86f
NBBJ\malexander <melissaalexander08@gmail.com>
2021-04-08 19:40:41 -0700

    Update sidebar.js

--- blocking-and-stacking-data-sync/src/sidebar.js
+++ blocking-and-stacking-data-sync/src/sidebar.js
@@ -53,7 +53,7 @@ function countBy(list, keyGetter) {
       const departmentSF = key[key.length - 1] === "" ? parseFloat(key[key.length - 2].replace(",","")) : parseFloat(key[key.length - 1].replace(",",""))
       const departmentName = key[key.length - 1] === "" ? key.slice(0,key.length - 2).join(" ") : key.slice(0,key.length - 1).join(" ")
       const count = map.get(departmentName)
-      map.set(departmentName, !count ? 0 : count + departmentSF)
+      map.set(departmentName, !count ? departmentSF : count + departmentSF)
   })
   return new Map([...map.entries()].sort((a, b) => b[1] - a[1]))
 }
